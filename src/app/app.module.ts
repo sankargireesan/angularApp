71a76b1329e6f3142a77182d4731675c393b5730
Gireesan Nair <sankar.gireesannair@philips.com>
2018-01-23 16:32:49 -0500

    lab 3'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,38 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { CollectionComponent } from './collection/collection.component';
+
+import  {
+    MatListModule,  MatCardModule,  MatSlideToggleModule,  MatDialogModule,
+    MatIconModule,  MatInputModule,  MatSnackBarModule,  MatTabsModule,
+    MatButtonModule,  MatLineModule,  MatToolbarModule
+}  from  '@angular/material';
+import  {  BrowserAnimationsModule  }  from  '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CollectionComponent,
     AboutComponent
   ],
-  imports: [
-    BrowserModule
+  imports:  [
+    BrowserModule,
+    FormsModule,
+    MatListModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatCardModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatLineModule,
+    MatInputModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
