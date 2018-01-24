7d5c3e03b8426140cca31c210460db351921e0cd
sankargireesan <sankar.gireesannair@philips.com>
2018-01-24 11:50:25 -0500

    lab 6

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ibook } from '../ibook';
 import { MatSnackBar } from '@angular/material';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'my-collection',
@@ -9,7 +10,7 @@ import { MatSnackBar } from '@angular/material';
 })
 export class CollectionComponent implements OnInit {
 
-  constructor(private _snackBar: MatSnackBar) {
+  constructor(private _snackBar: MatSnackBar, private _dataService: DataService) {
     this.openingTime = new Date();
     this.openingTime.setHours(10, 0);
     this.closingTime = new Date();
@@ -17,39 +18,10 @@ export class CollectionComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.books = this._dataService.getBooks();
   }
   pageTitle: string = 'My Collections'
-  books: Array<Ibook> =
-  [
-    {
-      id: 1,
-      title: "JavaScript - The Good Parts",
-      author: "Douglas Crockford",
-      isCheckedOut: true,
-      rating: 3
-    },
-    {
-      id: 2,
-      title: "The Wind in the Willows",
-      author: "Kenneth Grahame",
-      isCheckedOut: false,
-      rating: 4
-    },
-    {
-      id: 3,
-      title: "Pillars of the Earth",
-      author: "Ken Follett",
-      isCheckedOut: true,
-      rating: 5
-    },
-    {
-      id: 4,
-      title: "Harry Potter and the Prisoner of Azkaban",
-      author: "J. K. Rowling",
-      isCheckedOut: false,
-      rating: 5
-    }
-  ];
+  books: Array<Ibook>;
   showOperatingHours: boolean = false;
   openingTime: Date;
   closingTime: Date;
